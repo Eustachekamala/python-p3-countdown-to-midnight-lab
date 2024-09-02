75a1e8135519dc9d90e363454a3747e506e337c6
eustachekamala <eustachejared25@gmail.com>
2024-09-02 23:16:49 +0300

    update of the countdown function

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,6 +1,8 @@
+import time
 def countdown(start):
     while start > 0:
-        print(f'{start} SECOND(S)!')
+        print(f'# => {start} SECOND(S)!')
+        time.sleep(1)
         start -= 1
     print("HAPPY NEW YEAR!")
 
