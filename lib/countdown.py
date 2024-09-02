313ee59641914d957b25874961c7caa46f08fb8a
eustachekamala <eustachejared25@gmail.com>
2024-09-02 23:11:00 +0300

    I used the decrement methode with while lood

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
-# your code goes here!
+def countdown(start):
+    while start > 0:
+        print(f'{start} SECOND(S)!')
+        start -= 1
+    print("HAPPY NEW YEAR!")
+
+
+# Example usage
+countdown(10)
+
+# print(countdown(10))
